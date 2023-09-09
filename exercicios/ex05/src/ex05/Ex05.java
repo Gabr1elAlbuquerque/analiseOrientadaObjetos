ddbc44268f0555f8129caf279cf23215172a684f
Gabriel Albuquerque <96128995+Gabr1elAlbuquerque@users.noreply.github.com>
2023-09-09 15:44:06 -0300

    Update Ex05.java

--- exercicios/ex05/src/ex05/Ex05.java
+++ exercicios/ex05/src/ex05/Ex05.java
@@ -5,7 +5,7 @@ public class Ex05 {
 
     public static void main(String[] args) {
        Pessoa p1 = new Pessoa();
-      // p1.nome = "Gabriel"; esse código gera um erro, pois o atributo é private, sendo assim não é possivel modificá-lo fora da classe
+      // p1.nome = "Gabriel"; este código gera um erro, pois o atributo é private, sendo assim não é possivel modificá-lo fora da classe
       //ATRIBUTOS PRIVATE E METODOS PUBLIC SEMPRE! É UMA BOA PRÁTICA
        p1.setNome("Gabriel");
     }
